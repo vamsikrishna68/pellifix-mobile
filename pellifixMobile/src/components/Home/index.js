031d4abed18a4e50299f10593bea8cf9ac5b24cc
AppsWise Technologies Pvt. Ltd <sabareesh.addepalli@appswise.com>
2023-01-08 23:27:59 +0530

    home page integration

--- pellifixMobile/src/components/Home/index.js
+++ pellifixMobile/src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, Dimensions, Image, ScrollView} from 'react-native';
 import {
   Card,
@@ -93,154 +93,164 @@ const Home = () => {
   });
 
   useEffect(() => {
-    getDailyProfiles()
-    getHoroscopicProfiles()
-    getPreferenceProfiles()
+    getDailyProfiles();
+    getHoroscopicProfiles();
+    getPreferenceProfiles();
   });
 
   const getDailyProfiles = async () => {
     const response = await getProfiles('daily');
-    if(response){
-        setDailyRecommendation(response.data)
+    if (response) {
+      console.log(response.data, 'dat');
+      setDailyRecommendation(response.data);
     }
   };
 
-
   const getHoroscopicProfiles = async () => {
     const response = await getProfiles('horoscopic');
-    if(response){
-        setHoroscopeMatches(response.data)
+    if (response) {
+      setHoroscopeMatches(response.data);
     }
   };
 
   const getPreferenceProfiles = async () => {
     const response = await getProfiles('preference');
-    if(response){
-        setPreferenceMatches(response.data)
+    if (response) {
+      setPreferenceMatches(response.data);
     }
   };
-
-  
 
   return (
     <ScrollView style={styles.container}>
       <Text style={{marginVertical: 15}} variant="titleLarge">
         Daily Recommendations
       </Text>
-      <View style={{height: 500}}>
-        <SwiperFlatList
-          autoplay
-          autoplayLoop
-          index={0}
-          data={profiles}
-          renderItem={({item}) => (
-            <Card elevation={4}>
-              <Card.Title
-                style={{width: width - 15}}
-                title={item.name}
-                subtitle="September 14, 2016"
-                right={props => (
-                  <IconButton
-                    {...props}
-                    icon="cards-heart-outline"
-                    onPress={() => {}}
-                  />
-                )}
-              />
+      {dailyRecommendation && dailyRecommendation.length ? (
+        <View style={{height: 500}}>
+          <SwiperFlatList
+            autoplay
+            autoplayLoop
+            index={0}
+            data={dailyRecommendation}
+            renderItem={({item}) => (
+              <Card elevation={4}>
+                <Card.Title
+                  style={{width: width - 15}}
+                  title={item.name}
+                  subtitle="September 14, 2016"
+                  right={props => (
+                    <IconButton
+                      {...props}
+                      icon="cards-heart-outline"
+                      onPress={() => {}}
+                    />
+                  )}
+                />
 
-              <Card.Cover
-                style={{width: width, height: 300}}
-                source={item.img}
-              />
-              <Card.Content style={{width: width - 20}}>
-                <Paragraph>
-                  This impressive paella is a perfect party dish and a fun meal
-                  to cook together with your guests. Add 1 cup of frozen peas
-                  along with the mussels, if you like.
-                </Paragraph>
-              </Card.Content>
-            </Card>
-          )}
-        />
-      </View>
+                <Card.Cover
+                  style={{width: width, height: 300}}
+                  source={item.img}
+                />
+                <Card.Content style={{width: width - 20}}>
+                  <Paragraph>
+                    This impressive paella is a perfect party dish and a fun
+                    meal to cook together with your guests. Add 1 cup of frozen
+                    peas along with the mussels, if you like.
+                  </Paragraph>
+                </Card.Content>
+              </Card>
+            )}
+          />
+        </View>
+      ) : (
+        <Paragraph>No Data found!</Paragraph>
+      )}
       <Text style={{marginVertical: 5}} variant="titleLarge">
         Horoscopic Matches
       </Text>
-      <View style={{height: 500}}>
-        <SwiperFlatList
-          autoplay
-          autoplayLoop
-          index={0}
-          data={profiles}
-          renderItem={({item}) => (
-            <Card elevation={4}>
-              <Card.Title
-                style={{width: width - 15}}
-                title={item.name}
-                subtitle="September 14, 2016"
-                right={props => (
-                  <IconButton
-                    {...props}
-                    icon="cards-heart-outline"
-                    onPress={() => {}}
-                  />
-                )}
-              />
+      {horoscopeMatches && horoscopeMatches.length ? (
+        <View style={{height: 500}}>
+          <SwiperFlatList
+            autoplay
+            autoplayLoop
+            index={0}
+            data={horoscopeMatches}
+            renderItem={({item}) => (
+              <Card elevation={4}>
+                <Card.Title
+                  style={{width: width - 15}}
+                  title={item.name}
+                  subtitle="September 14, 2016"
+                  right={props => (
+                    <IconButton
+                      {...props}
+                      icon="cards-heart-outline"
+                      onPress={() => {}}
+                    />
+                  )}
+                />
 
-              <Card.Cover
-                style={{width: width, height: 300}}
-                source={item.img}
-              />
-              <Card.Content style={{width: width - 20}}>
-                <Paragraph>
-                  This impressive paella is a perfect party dish and a fun meal
-                  to cook together with your guests. Add 1 cup of frozen peas
-                  along with the mussels, if you like.
-                </Paragraph>
-              </Card.Content>
-            </Card>
-          )}
-        />
-      </View>
+                <Card.Cover
+                  style={{width: width, height: 300}}
+                  source={item.img}
+                />
+                <Card.Content style={{width: width - 20}}>
+                  <Paragraph>
+                    This impressive paella is a perfect party dish and a fun
+                    meal to cook together with your guests. Add 1 cup of frozen
+                    peas along with the mussels, if you like.
+                  </Paragraph>
+                </Card.Content>
+              </Card>
+            )}
+          />
+        </View>
+      ) : (
+        <Paragraph>No Data found!</Paragraph>
+      )}
       <Text style={{marginVertical: 5}} variant="titleLarge">
         Preference Matches
       </Text>
-      <View style={{height: 800}}>
-        <SwiperFlatList
-          autoplay
-          autoplayLoop
-          index={0}
-          data={profiles}
-          renderItem={({item}) => (
-            <Card elevation={4}>
-              <Card.Title
-                style={{width: width - 15}}
-                title={item.name}
-                subtitle="September 14, 2016"
-                right={props => (
-                  <IconButton
-                    {...props}
-                    icon="cards-heart-outline"
-                    onPress={() => {}}
-                  />
-                )}
-              />
+      {preferenceMatches && preferenceMatches.length ? (
+        <View style={{height: 800}}>
+          <SwiperFlatList
+            autoplay
+            autoplayLoop
+            index={0}
+            data={dailyRecommendation}
+            renderItem={({item}) => (
+              <Card elevation={4}>
+                <Card.Title
+                  style={{width: width - 15}}
+                  title={item.name}
+                  subtitle="September 14, 2016"
+                  right={props => (
+                    <IconButton
+                      {...props}
+                      icon="cards-heart-outline"
+                      onPress={() => {}}
+                    />
+                  )}
+                />
 
-              <Card.Cover
-                style={{width: width, height: 300}}
-                source={item.img}
-              />
-              <Card.Content style={{width: width - 20}}>
-                <Paragraph>
-                  This impressive paella is a perfect party dish and a fun meal
-                  to cook together with your guests. Add 1 cup of frozen peas
-                  along with the mussels, if you like.
-                </Paragraph>
-              </Card.Content>
-            </Card>
-          )}
-        />
-      </View>
+                <Card.Cover
+                  style={{width: width, height: 300}}
+                  source={item.img}
+                />
+                <Card.Content style={{width: width - 20}}>
+                  <Paragraph>
+                    This impressive paella is a perfect party dish and a fun
+                    meal to cook together with your guests. Add 1 cup of frozen
+                    peas along with the mussels, if you like.
+                  </Paragraph>
+                </Card.Content>
+              </Card>
+            )}
+          />
+        </View>
+      ) : (
+        <Paragraph>No Data found!</Paragraph>
+      )}
     </ScrollView>
   );
 };
